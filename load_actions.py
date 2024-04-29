87bd32ba83a407586abd2f66d7e3402acf324a55
vaedrenn <brianto10622@gmail.com>
2024-04-28 22:57:00 -0400

    Implemented local loading of models

--- load_actions.py
+++ load_actions.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 import timm
+from torch import nn
 
 
 def load_model(model_path: str | os.path, filename="model.safetensors"):
@@ -16,19 +17,27 @@ def load_model(model_path: str | os.path, filename="model.safetensors"):
     :return: returns the loaded model
     """
     config_file = open(os.path.join(model_path, "config.json"))
-
     configs = json.load(config_file)
-    arch = configs['architecture']
+
     file_path = os.path.join(model_path, filename)
-    model = timm.create_model(
-        model_name=arch,
-        pretrained_cfg_overlay=dict(file=file_path)
-    )
+
+    model_name = configs["architecture"]
+    pretrained_cfg = configs['pretrained_cfg']
+    pretrained_cfg['num_classes'] = configs['num_classes']
+    model_args = configs['model_args']
+
+    model: nn.Module = timm.create_model(
+        model_name=model_name,
+        pretrained_cfg_overlay=pretrained_cfg,
+        checkpoint_path=file_path,
+        **model_args
+
+    ).eval()
 
     return model
 
 
-def load_labels(model_path: str | os.path, filename: str, categories: dict) -> dict:
+def load_labels(model_path: str | os.path, categories: dict, filename="selected_tags.csv") -> dict:
     """
     Loads labels from  txt file
     :param filename:name of label file
@@ -61,5 +70,8 @@ def load_labels(model_path: str | os.path, filename: str, categories: dict) -> d
 
 if __name__ == '__main__':
     path = r"wd-vit-tagger-v3"
+    test_dict = {"rating": 9, "general": 0, "characters": 4}
     r = load_model(path)
+    r2 = load_labels(path, filename="selected_tags.csv", categories=test_dict)
     print(r)
+    print(r2.keys())
