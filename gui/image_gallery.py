a8200563fa180cc78635f7f5500592aa06c33cf1
vaedrenn <brianto10622@gmail.com>
2024-06-01 09:35:17 -0400

    I have accepted there's no easy way to make a justified image gallery

--- gui/image_gallery.py
+++ gui/image_gallery.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt, QSize, QUrl, QRect
-from PyQt5.QtGui import QDesktopServices, QIcon
+from PyQt5.QtGui import QDesktopServices, QIcon, QPixmap
 from PyQt5.QtWidgets import QListWidget, QAbstractItemView, QListView, QStyledItemDelegate, QApplication, \
     QStyleOptionViewItem, QToolTip
 
@@ -15,7 +15,10 @@ class ImageGallery(QListView):
         self.setViewMode(QListWidget.IconMode)
         self.setAcceptDrops(False)
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)  # ctrl and shift click selection
-        self.setIconSize(QSize(180, 180))
+        self.setIconSize(QSize(800, 200))
+        self.setSpacing(5)
+        self.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.verticalScrollBar().setSingleStep(50)
         self.setResizeMode(QListWidget.Adjust)  # Reorganize thumbnails on resize
         self.doubleClicked.connect(self.open_image)
 
@@ -26,21 +29,6 @@ class ImageGallery(QListView):
         """
         file_path = self.model().data(index, Qt.DisplayRole)
         QDesktopServices.openUrl(QUrl.fromLocalFile(file_path))
-
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-        self.adjustSpacing()
-
-    def adjustSpacing(self):
-        icon_size = self.iconSize()
-        available_width = self.viewport().width()
-
-        num_columns = max(1, available_width // icon_size.width())
-        extra_space = available_width - (num_columns * icon_size.width())
-        new_spacing = max(5, extra_space // (num_columns + 1))
-
-        new_grid_size = QSize(icon_size.width() + new_spacing, 185)
-        self.setGridSize(new_grid_size)
 
 
 class ThumbnailDelegate(QStyledItemDelegate):
