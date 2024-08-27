a7eaa8c97b9018e79b56e64cdbbdcd8e07a1cd2f
vaedrenn <brianto10622@gmail.com>
2024-08-27 16:07:35 -0400

    test changing values for thresholds

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -16,6 +16,12 @@ class TestMainWindow(TestCase):
         self.app.quit()
 
     def test_submit_button_click(self):
+        """
+        Test if the submit dialog shows up
+        """
+        categories = self.window.center_widget.categories
+        thresholds = self.window.center_widget.threshold
+
         # Find the submit button in the navigation bar
         submit_button = None
         for btn in self.window.center_widget.findChildren(QPushButton):
@@ -38,11 +44,35 @@ class TestMainWindow(TestCase):
         # Simulate a button click
         QTest.mouseClick(submit_button, Qt.LeftButton)
 
+        dialog = None
+
+        for widget in QApplication.topLevelWidgets():
+            if isinstance(widget, QDialog) and widget.windowTitle() == "Set Thresholds":
+                dialog = widget
+
+        # Check if the dialog initializes with the correct values
+        for category, value in thresholds.items():
+            spinbox = dialog.spinboxes[category]
+            self.assertEqual(spinbox.value(), int(value * 100), f"{category} spinbox did not initialize correctly.")
+
+        # Test if changing thresholds works
+        category = 'rating'
+        new_value = 90  # New threshold value
+        dialog.spinboxes[category].setValue(new_value)
+        self.assertEqual(thresholds[category], new_value / 100.0,
+                         f"{category} threshold was not updated correctly.")
+
+        new_value = 50  # change it back
+        dialog.spinboxes[category].setValue(new_value)
+        self.assertEqual(thresholds[category], new_value / 100.0,
+                         f"{category} threshold was not updated correctly.")
+
         # Since the dialog is modal and blocks, the test will only continue after the dialog is closed
         # Add an assertion to verify the dialog was successfully closed
         for widget in QApplication.topLevelWidgets():
             if isinstance(widget, QDialog) and widget.windowTitle() == "Set Thresholds":
                 self.assertFalse(widget.isVisible(), "ThresholdDialog is still visible!")
+
 
 
 if __name__ == "__main__":
